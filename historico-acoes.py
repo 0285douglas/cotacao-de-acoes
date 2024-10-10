ed5a4d4b778730094052c03d516c01f80fc09c2c
0285douglas <0285douglas@gmail.com>
2024-10-10 19:24:06 -0300

    Organização de código
    Organizado e adicionado comentários ao código para melhor entendimento

--- historico-acoes.py
+++ historico-acoes.py
@@ -2,19 +2,17 @@ import requests # framework de requisição de API
 import pandas as pd # framework para criação e manipulação de Dataframes
 from io import StringIO # conversor de arquivos text para csv
 
+api = 'FGO0YEX3QWG8VK3K' # chave api/api key
+acao = 'ITUB3'  # Ação que os dados serão coletados
+url = f'https://www.alphavantage.co/query?function=TIME_SERIES_DAILY&symbol={acao}.SAO&apikey={api}&datatype=csv'   # API
 
-api = 'FGO0YEX3QWG8VK3K' # chave api / api key
-acao = 'ITUB3'
+# Requisição 
+r = requests.get(url) 
 
-url = f'https://www.alphavantage.co/query?function=TIME_SERIES_DAILY&symbol={acao}.SAO&apikey={api}&datatype=csv'
-r = requests.get(url)
+# Conversão de arquivo text para csv
+tabela = pd.read_csv(StringIO(r.text))    
+print(tabela)
 
-print(r.text)
-
-
-
-tabela = pd.read_csv('r.csv')
-display(tabela)
 
 
 
